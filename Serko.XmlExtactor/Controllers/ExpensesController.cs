24c7f868009f9117cd883bcf1d50b7c2646ac67b
Piyush Dhimate <bitartist@hotmail.com>
2019-03-16 14:50:07 +0530

    Added expense scontroller

---
+++ Serko.XmlExtactor/Controllers/ExpensesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serko.XmlExtractor.Business.Services;
+
+namespace Serko.XmlExtactor.Controllers
+{
+    [Route("api/[controller]")]
+    public class ExpensesController : Controller
+    {
+        private readonly IExpenseService _expenseService;
+
+        public ExpensesController(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        // POST api/expenses
+        [HttpPost]
+        public IActionResult Post([FromBody]string textWithXml)
+        {
+            var report = _expenseService.GetExpenseReport(textWithXml);
+            return Ok(report);
+        }
+
+    }
+}
